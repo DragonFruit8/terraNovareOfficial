09f39fec40618a7339857a38eca2cfed16914019
DragonFruit8 <Jtbyers19@gmail.com>
2025-03-01 19:04:01 -0500

    server.js x-forwarded

--- backend/server.js
+++ backend/server.js
@@ -50,12 +50,12 @@ app.use((req, res, next) => {
 });
 
 
-// app.use((req, res, next) => {
-//   if (req.headers["x-forwarded-proto"] !== "https") {
-//     return res.redirect("https://" + req.headers.host + req.url);
-//   }
-//   next();
-// });
+app.use((req, res, next) => {
+  if (req.headers["x-forwarded-proto"] !== "https") {
+    return res.redirect("https://" + req.headers.host + req.url);
+  }
+  next();
+});
 
 // ✅ Print all routes when server starts
 
