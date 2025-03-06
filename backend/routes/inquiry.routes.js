895c81b5e5d550b05c21f5d94ac042da5391298b
DragonFruit8 <Jtbyers19@gmail.com>
2025-03-06 13:28:55 -0500

    Opps

--- backend/routes/inquiry.routes.js
+++ backend/routes/inquiry.routes.js
@@ -1,9 +1,9 @@
 import express from "express";
-const router = express.Router();
-import handleWebDevInquiry from "../controllers/webdev.controller.js"
-import handleDJInquiry from "../controllers/djservice.controller.js"
-import handleSponsorInquiry from "../controllers/donorsponsor.controller.js"
+import { handleWebDevInquiry } from "../controllers/webdev.controller.js"
+import { handleDJInquiry } from "../controllers/djservice.controller.js"
+import { handleSponsorInquiry } from "../controllers/donorsponsor.controller.js"
 
+const router = express.Router();
 
 router.post("/webdev", handleWebDevInquiry);
 router.post("/dj", handleDJInquiry);
