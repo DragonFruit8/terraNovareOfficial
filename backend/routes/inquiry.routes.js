9baa677d28b42678ec302906b667db370944271a
DragonFruit8 <Jtbyers19@gmail.com>
2025-03-06 13:24:23 -0500

    module vs. regular file output

--- backend/routes/inquiry.routes.js
+++ backend/routes/inquiry.routes.js
@@ -1,8 +1,8 @@
 import express from "express";
 const router = express.Router();
-import { handleWebDevInquiry } from "../controllers/webdev.controller.js"
-import { handleDJInquiry } from "../controllers/djservice.controller.js"
-import { handleSponsorInquiry } from "../controllers/donorsponsor.controller.js"
+import handleWebDevInquiry from "../controllers/webdev.controller.js"
+import handleDJInquiry from "../controllers/djservice.controller.js"
+import handleSponsorInquiry from "../controllers/donorsponsor.controller.js"
 
 
 router.post("/webdev", handleWebDevInquiry);
